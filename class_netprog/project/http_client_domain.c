bd45923ee2d8f2f33ee1b6928487a8bac1e1de17
kkambbak <71304637+kkambbak@users.noreply.github.com>
2021-12-12 00:08:43 +0900

    Add files via upload

---
+++ class_netprog/project/http_client_domain.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#define BUF_SIZE 1024
+void error_handling(char *message);
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    char message[BUF_SIZE];
+    char message1[BUF_SIZE];
+    char msg[BUF_SIZE] = "GET / HTTP/1.1\r\nUser-Agent: Mozila/4.0\r\ncontent-type:text/html\r\nConnection:close\r\n\r\n";
+    int str_len;
+    struct sockaddr_in serv_adr;
+    struct hostent *remoteHost;
+
+    if(argc!=3) {
+        printf("Usage : %s <IP> <port>\n", argv[0]);
+        exit(1);
+    }
+
+    sock=socket(PF_INET, SOCK_STREAM, 0);
+    if(sock==-1)
+        error_handling("socket() error");
+
+    remoteHost=gethostbyname(argv[1]);
+
+    memset(&serv_adr, 0, sizeof(serv_adr));
+    serv_adr.sin_family=AF_INET;
+    serv_adr.sin_addr.s_addr=inet_addr(inet_ntoa(*(struct in_addr*)*remoteHost->h_addr_list));
+    serv_adr.sin_port=htons(atoi(argv[2]));
+
+    if(connect(sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr))==-1)
+        error_handling("connect() error!");
+    else
+        puts("Connected...........");
+     
+    send(sock, msg, strlen(msg),0);
+   
+    while(1) 
+    {
+        write(sock, message, strlen(message));
+        str_len=read(sock, message, BUF_SIZE-1);
+        message[str_len]=0;
+        printf("%s", message);
+    }
+
+    close(sock);
+    return 0;
+}
+
+void error_handling(char *message)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+    exit(1);
+}
+
